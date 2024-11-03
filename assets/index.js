b52be4d823b64b12991469aca014c8cd2c8b6d64
belunatic <abelluvyou@yahoo.com>
2024-11-03 14:35:43 -0800

    created a func to add the data to a an object and another to submit the data to localstorage

--- assets/index.js
+++ assets/index.js
@@ -2,10 +2,37 @@
 let data = null;
 
 //Check to see if our localstorage has been initiated
-if (!localStorage.getItem("journalData")) {
+if (!localStorage.getItem("journalData") || !data) {
 	//initilize the localstorage
 	localStorage.setItem("journalData", data);
+	data = [];
 } else {
 	//return the data in a JSON format
 	data = JSON.parse(localStorage.getItem("journalData"));
+}
+
+//set the journal data
+function submittedJournalData() {
+	//initiate the object
+	let dataSubmitted = {};
+	//id
+	dataSubmitted.id = Date.now();
+	//date
+	dataSubmitted.submittedDate = new Date().toLocaleString();
+	//text
+	dataSubmitted.text = document.querySelector("#message").value;
+	//if ever updated mark the date
+	dataSubmitted.dateUpdated = null;
+
+	return dataSubmitted;
+}
+
+//submit the journal
+document.querySelector(".submit").addEventListener("click", submitJournal);
+
+function submitJournal() {
+	//push the new journal data tot he fornt of the data array
+	data.unshift(submittedJournalData());
+	//add data tot he localstorage
+	localStorage.setItem("journalData", JSON.stringify(data));
 }
