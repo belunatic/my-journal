117e315c3ad4b7e482c37d713dde890f79ab36b0
belunatic <abelluvyou@yahoo.com>
2024-11-12 15:04:23 -0800

    click new will open the dialog and changed the new button id

--- assets/js/dialog.js
+++ assets/js/dialog.js
@@ -2,8 +2,15 @@
 let dialogBox = document.querySelector("dialog");
 // retrieve the cancel button
 let cancelButton = document.querySelector(".cancelEntry");
+//retrieve the new journal entry button
+let newJournalEntryButton = document.querySelector("#newJournalEntry");
 
 //close the dialog
 cancelButton.addEventListener("click", () => {
 	dialogBox.close();
+});
+
+//open dialog to add journal entry
+newJournalEntryButton.addEventListener("click", () => {
+	dialogBox.showModal();
 });
