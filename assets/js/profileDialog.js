7bb09fd874e39cc47d167d768fc56fc1f21df8af
belunatic <abelluvyou@yahoo.com>
2024-11-19 11:06:47 -0800

    profile Dialog setup, class chnaged names to adopt to profile js

---
+++ assets/js/profileDialog.js
@@ -0,0 +1,33 @@
+//get the profile dialog form the DOM
+let profileDialog = document.querySelector("#profile");
+//get the cancel button
+let cancelProfileButton = document.querySelector(".cancelProfile");
+
+//check to see if profile has any value
+//if not display the pop-up
+if (!localStorage.getItem("profileData")) {
+	//hide the cancel button if profile data is empty
+	cancelProfileButton.classList.add("hidden");
+	profileDialog.showModal();
+}
+
+//submit event listener function
+function submitProfile() {
+	let profileName = document.querySelector("#name").value.trim();
+	if (!profileName.length) {
+		document.querySelector(".error-name").classList.remove("hidden");
+	} else {
+		document.querySelector(".error-name").classList.add("hidden");
+		localStorage.setItem("profileData", profileName);
+		profileDialog.close();
+	}
+}
+
+//submit the profile data
+document
+	.querySelector(".submitProfile")
+	.addEventListener("click", submitProfile);
+
+document.querySelector(".cancelProfile").addEventListener("click", () => {
+	profileDialog.close();
+});
