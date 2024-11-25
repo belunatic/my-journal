54351402c2cdbecdd84e943d71a75064f2088ab3
belunatic <abelluvyou@yahoo.com>
2024-11-25 10:54:48 -0800

    added the edit button logic and change the name when it gets updated

--- assets/js/profileDialog.js
+++ assets/js/profileDialog.js
@@ -2,6 +2,10 @@
 let profileDialog = document.querySelector("#profile");
 //get the cancel button
 let cancelProfileButton = document.querySelector(".cancelProfile");
+//get the edit button
+let editProfileButton = document.querySelector("#editProfileButton");
+//get the input in the profile dialog
+let profileTextInput = document.querySelector("input#name");
 
 //check to see if profile has any value
 //if not display the pop-up
@@ -25,9 +29,20 @@ function submitProfile() {
 	} else {
 		document.querySelector(".error-name").classList.add("hidden");
 		localStorage.setItem("profileData", profileName);
+		//update the name site wise
+		document.querySelector(".profileName").innerText = profileName;
 		profileDialog.close();
 	}
 }
+
+//edit Profile Button
+editProfileButton.addEventListener("click", () => {
+	//add value to the input
+	//this will work because "Edit Button" only shows when localStorage has data
+	profileTextInput.value = localStorage.getItem("profileData");
+	// open the profile dialog
+	profileDialog.showModal();
+});
 
 //submit the profile data
 document
